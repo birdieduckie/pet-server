51856d48d0a66da7457453c4cecddcc1e0fad222
Natasha S <vrthngcnts@gmail.com>
2022-07-01 18:09:27 +0300

    database connected, up for refactoring

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require('mongodb')
+
 const Db = process.env.ATLAS_URI
+
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -12,7 +14,7 @@ module.exports = {
     client.connect(function (err, db) {
       // Verify we got a good "db" object
       if (db) {
-        _db = db.db('users')
+        _db = db.db('PetPets')
         console.log('Successfully connected to MongoDB.')
       }
       return callback(err)
--- routes/users.js
+++ routes/users.js
@@ -6,40 +6,41 @@ const dbo = require('../db/conn')
 
 const ObjectId = require('mongodb').ObjectId
 
-userRoutes.route('/users').get((req, res) => {
-  let db_connect = dbo.getDb('users')
-  db_connect
+userRoutes.route('/users').get(function (req, res) {
+  let db_connect = dbo.getDb('PetPets')
+  let users = db_connect
     .collection('users')
     .find()
-    .toArray((err, result) => {
+    .toArray(function (err, result) {
       if (err) throw err
       res.json(result)
       console.log(result)
     })
+  console.log(users)
 })
 
-userRoutes.route('/user/:id').get((req, res) => {
+userRoutes.route('/user/:id').get(function (req, res) {
   let db_connect = dbo.getDb()
   let myquery = { _id: ObjectId(req.params.id) }
-  db_connect.collection('records').findOne(myquery, (err, result) => {
+  db_connect.collection('users').findOne(myquery, function (err, result) {
     if (err) throw err
     res.json(result)
   })
 })
 
-userRoutes.route('/user/add').post((req, response) => {
+userRoutes.route('/user/add').post(function (req, response) {
   let db_connect = dbo.getDb()
   let myobj = {
     name: req.body.name,
     surname: req.body.surname,
   }
-  db_connect.collection('records').insertOne(myobj, (err, res) => {
+  db_connect.collection('records').insertOne(myobj, function (err, res) {
     if (err) throw err
     response.json(res)
   })
 })
 
-userRoutes.route('/update/:id').post((req, response) => {
+userRoutes.route('/update/:id').post(function (req, response) {
   let db_connect = dbo.getDb()
   let myquery = { _id: ObjectId(req.params.id) }
   let newvalues = {
@@ -53,7 +54,7 @@ userRoutes.route('/update/:id').post((req, response) => {
 userRoutes.route('/:id').delete((req, response) => {
   let db_connect = dbo.getDb()
   let myquery = { _id: ObjectId(req.params.id) }
-  db_connect.collection('users').deleteOne(myquery, (err, obj) => {
+  db_connect.collection('users').deleteOne(myquery, function (err, obj) {
     if (err) throw err
     console.log('1 document deleted')
     response.json(obj)
